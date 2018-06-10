8108ab733a205ceb720d8cc331fbf24f88544317
vincentwzx <miracle19920111@gmail.com>
2018-06-10 17:05:44 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.wpy files
+  plugins: [
+    'html'
+  ],
+  settings: {
+    'html/html-extensions': ['.html', '.wpy']
+  },
+  // add your custom rules here
+  'rules': {
+    "indent": ["error", "tab"],
+    "no-tabs": 0,
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'eqeqeq': 0,
+    'no-useless-return': 0,
+    'no-unreachable': 0
+  },
+  globals: {
+    wx: false,
+    getCurrentPages: false
+  }
+}
